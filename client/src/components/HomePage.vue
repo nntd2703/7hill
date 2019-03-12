973704636ca0afc249416eb7c76e36ca01cd8d7f
Dat Nguyen <dat.nguyen@gbst.com>
2019-03-12 17:55:34 +0700

    'abc'

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="HomePage">
-    <nav-top></nav-top>
-    <navigation-bar></navigation-bar>
-    <slide-show/>
-    <panel-our-service/>
-    <panel-our-products/>
-    <panel-contact/>
-    <panel-content-about/>
-    <founder-panel/>
-    <panel-categories></panel-categories>
-    <panel-footer></panel-footer>
+    <div class="loading w-100 h-100" v-if="loading">
+      <div class="item">
+        <cube-shadow></cube-shadow>
+      </div>
+    </div>
+    <div v-else>
+      <nav-top></nav-top>
+      <navigation-bar></navigation-bar>
+      <slide-show/>
+      <panel-our-service/>
+      <panel-our-products :listItem1="listItem1" :listItem2="listItem2"/>
+      <panel-contact/>
+      <panel-content-about/>
+      <founder-panel/>
+      <panel-categories></panel-categories>
+      <panel-footer></panel-footer>
+    </div>
   </div>
 </template>
 
@@ -25,17 +32,72 @@ import NavTop from './Homepage/navTop'
 import NavigationBar from './Homepage/navigationBar'
 import PanelCategories from './Homepage/panelCategories'
 import PanelFooter from './Homepage/panelFooter'
+import CubeShadow from "vue-loading-spinner/src/components/Circle9";
+import {firebase} from '@/services/firebaseConfig'
 
 export default {
   name: 'app',
-  components: { PanelFooter, PanelCategories, NavigationBar, NavTop, FounderPanel, PanelContentAbout, PanelContact, PanelOurProducts, PanelOurService, SlideShow },
+  components: {CubeShadow, PanelFooter, PanelCategories, NavigationBar, NavTop, FounderPanel, PanelContentAbout, PanelContact, PanelOurProducts, PanelOurService, SlideShow },
+  watch: {
+    listItemProduct() {
+      this.loading = true
+      let count = this.listItemProduct.length
+      let key = 0
+      this.listItemProduct.forEach((item) => {
+        if (key <= count/2 || count <= 3) {
+          this.listItem1.push(item)
+          key++
+        } else {
+          this.listItem2.push(item)
+          key++
+        }
+      })
+      setTimeout(() => {
+        if(this.isLoadingDataDone) {
+          this.loading = false
+        }
+      }, 1700)
+    },
+    isLoadingDataDone () {
+      if(this.isLoadingDataDone) {
+        this.loading = false
+      }
+    }
+  },
   created () {
     this.$emit(`update:layout`, HomeIndex)
+    this.ref.onSnapshot((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.listItemProduct.push({
+          'id': doc.id,
+          'key': doc.data().key,
+          'name': doc.data().name,
+          'imageUrl': doc.data().imageUrl
+        })
+      })
+    })
+    this.isLoadingDataDone = true
+  },
+  data () {
+    return {
+      listItemProduct: [],
+      listItem1: [],
+      listItem2: [],
+      loading: true,
+      isLoadingDataDone: false,
+      ref: firebase.firestore().collection('items')
+    }
   }
 }
 
 </script>
 
 <style lang="scss">
-
+.loading {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
 </style>
--- client/src/components/Homepage/panelOurProducts.vue
+++ client/src/components/Homepage/panelOurProducts.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="panelExpertise paral">
-
     <header-component headerLabel="Out Products" class="colorSF pt-md-3"/>
     <div class="container p-md-0 pb-3">
       <div class="slideShowProduct">
         <div class="carousel-wrap pb-5 d-md-block d-none">
-          <carousel  :loop="true" :autoplay="true" :autoplayTimeout="8000" :perPageCustom="[[480, 2], [768, 3]]"  :perPage="1" :navigationEnabled="true" :paginationEnabled="false" :navigationPrevLabel="null" :navigationNextLabel="null" :speed="2000" class="d-flex justify-content-around" >
+          <carousel  :loop="true" :autoplay="true" :autoplayTimeout="8000" :per-page-custom="[[768, 3]]" :navigation-enabled="false" :paginationEnabled="false" :speed="2000" class="d-flex justify-content-around" >
             <slide class="item" v-for="item in listItem1" v-bind:key="item['.key']">
               <img class="position-relative" :src="item.imageUrl" alt="">
               <h3 class="position-absolute text-white h3CenterDiv">{{item.name}}</h3>
@@ -13,7 +12,7 @@
           </carousel>
         </div>
         <div class="carousel-wrap pb-5 d-md-block d-none">
-          <carousel  :loop="true" :autoplay="true" :autoplayTimeout="8000" :perPageCustom="[[480, 2], [768, 3]]" :perPage="1" :navigationEnabled="true" :paginationEnabled="false" :navigationPrevLabel="null" :navigationNextLabel="null" :speed="2000" class="d-flex justify-content-around">
+          <carousel  :loop="true" :autoplay="true" :autoplayTimeout="8000" :per-page-custom="[[768, 3]]" :navigationEnabled="true" :paginationEnabled="false" :navigationPrevLabel="null" :navigationNextLabel="null" :speed="2000" class="d-flex justify-content-around">
             <slide class="item" v-for="item in listItem2" v-bind:key="item['.key']">
               <img class="position-relative" :src="item.imageUrl" alt="">
               <h3 class="position-absolute text-white h3CenterDiv">{{item.name}}</h3>
@@ -30,49 +29,24 @@
   import 'owl.carousel/dist/assets/owl.carousel.css'
   import 'owl.carousel/dist/assets/owl.theme.default.min.css'
   import 'owl.carousel'
-  import {firebase} from '@/services/firebaseConfig'
 
   export default {
     name: 'panelOurProducts',
     components: { HeaderComponent },
     watch: {
-      listItemProduct() {
-        let count = this.listItemProduct.length
-        let key = 0
-        this.listItemProduct.forEach((item) => {
-          if (key <= count/2 || count <= 3) {
-            this.listItem1.push(item)
-            key++
-          } else {
-            this.listItem2.push(item)
-            key++
-          }
-        })
-      }
     },
+    props: [
+      'listItem1',
+      'listItem2'
+    ],
     data() {
       return {
-        listItemProduct: [],
-        listItem1: [],
-        listItem2: [],
-        errors: [],
-        ref: firebase.firestore().collection('items'),
       }
     },
     mounted() {
-
     },
     created() {
-      this.ref.onSnapshot((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          this.listItemProduct.push({
-            'id': doc.id,
-            'key': doc.data().key,
-            'name': doc.data().name,
-            'imageUrl': doc.data().imageUrl
-          })
-        })
-      })
+
     },
     methods: {
     }
@@ -84,21 +58,31 @@
     background-image: url("../../assets/backgroundProduct.jpg");
     .item img{
       width: 90%;
-      max-width: 380px;
       max-height: 223px !important;
     }
     .h3CenterDiv {
       font-size: 1.4rem;
       top: 46%;
       padding-left: 113px;
-      @media (max-width: 1200px) {
+    }
+    @media (max-width: 1200px) {
+      .h3CenterDiv {
         font-size: 1.2rem;
       }
-      @media (max-width: 991px) {
+    }
+    @media (max-width: 991px) {
+      .h3CenterDiv {
         font-size: 1rem;
       }
-      @media (max-width: 768px) {
+    }
+    @media (max-width: 768px) {
+      .h3CenterDiv {
         font-size: 2rem;
+      }
+    }
+    @media (max-width: 600px) {
+      .item img {
+        width: 50%;
       }
     }
   }
